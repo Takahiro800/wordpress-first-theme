fa012a682c572f2157c8ecff01c397ecb04d3100
Takahiro800 <52635809+Takahiro800@users.noreply.github.com>
2020-04-03 10:44:53 +0900

    自作テーマの開始

---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title><?php bloginfo('name'); ?>/</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="ここにサイト説明を入れます">
+<meta name="keywords" content="キーワード１,キーワード２,キーワード３,キーワード４,キーワード５">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/slide.css">
+<script type="text/javascript" src="js/openclose.js"></script>
+<script type="text/javascript" src="js/fixmenu_pagetop.js"></script>
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+
+<body>
+
+<header>
+<div class="inner">
+	<h1 id="logo">
+		<a href="<?php echo home_url(); ?>">
+			<?php bloginfo('name');  ?>
+		</a>
+	</h1>
+	<h2 id="description">
+		<p>
+			<?php bloginfo('description'); ?>
+		</p>
+	</h2>
+<div id="contact">
+
+<p class="tel">TEL:00-0000-0000<span>受付：00:00〜00:00　定休日：水曜日</span></p>
+</div>
+</div>
+</header>
