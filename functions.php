c6c518d86049ad21745958d2994b5bf744703f35
Takahiro800 <52635809+Takahiro800@users.noreply.github.com>
2020-04-03 11:54:53 +0900

    各ページのタブの表示を修正

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+add_theme_support( 'title-tag' );
+add_filter('document_title_separator', 'my_document_title_separator');
+function my_document_title_separator($separator){
+  $separator = '|';
+  return $separator;
+}
