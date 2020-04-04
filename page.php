9dae6b229ea5ee7517d384bbac0b9c3fad8250c0
Takahiro800 <52635809+Takahiro800@users.noreply.github.com>
2020-04-04 18:24:11 +0900

    固定ページ「採用情報」へのリンク作成

---
+++ page.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+
+
+
+<?php if (have_posts() ): ?>
+<?php while( have_posts() ) : the_post(); ?>
+<section>
+<h2><?php the_title(); ?></h2>
+<table class="ta1">
+	<caption>正社員募集</caption>
+	<?php for ($count = 0; $count < 6; $count++){ ?>
+	<tr>
+	<th>見出し</th>
+	<td><a href="#"><?php the_content(); ?></a></td>
+	</tr>
+<?php } ?>
+</table>
+	<?php endwhile; ?>
+	<?php endif; ?>
+
+
+
+<!--/#main-->
+
+
+<?php get_footer(); ?>
+
+
+
+</body>
+</html>
