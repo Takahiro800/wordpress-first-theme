f88a92cc526b6991cee665597e3051cd37c53cf7
Takahiro800 <52635809+Takahiro800@users.noreply.github.com>
2020-04-06 14:19:25 +0900

    左 アクセス　sidebar へ移行

--- page-link.php
+++ page-link.php
@@ -39,13 +39,7 @@
 
 			<?php get_sidebar('product'); ?>
 
-			<div class="box1">
-				<h2>アクセス</h2>
-				<p>東京都XX区XXXXビル３F<br>
-					TEL：03-0000-0000<br>
-					受付：9:00～16:00<br>
-					定休日：土日祝</p>
-			</div>
+			<?php get_sidebar('access'); ?>
 
 		</div>
 		<!--/#sub-->
